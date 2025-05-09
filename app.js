183d283c2a4c116540f352bc113d84cd7d09b580
santiagodiaz21 <fsantiagodiazc21@gmail.com>
2025-05-09 18:26:08 -0500

    commit para despliegue

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("Public"))
+
 
 app.use("/api/categoria", categoria);
 app.use("/api/inventario", inventario);
